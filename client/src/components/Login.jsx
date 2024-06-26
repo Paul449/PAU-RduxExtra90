029f90d5a05e3608fecd1fb1a0cf096b11d45817
paul449 <paul_bilbatua@hotmail.com>
2024-06-26 12:05:36 -0500

    comp

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,19 @@
+import {useState} from 'react'
+
+function Login(){
+
+    return(<>
+    <div className='Login-form'>
+        <h3 id='L'>Login</h3>
+        <form action=''>
+        <label>Username</label>
+        <input type='text'></input>
+        <label>Password</label>
+        <input type='password'></input>
+        </form>
+        <button type='submit'>Go</button>
+        <p>forgot password?<a>click on here!!!</a></p>
+    </div>
+    </>)
+
+}
---
+++ client/src/components/signUp.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+
+function signUp(){
+    const [FirstName, setFirstName] = useState('');
+    const [LastName, setLastName] = useState('');
+    const [Email, setEmail] = useState('');
+    const [Password, setPassword] = useState('');
+    return(<>
+    <div className='sign-form'>
+        <h3 id='s'>sign-up</h3>
+        <form action=''>
+        <label>First Name</label>
+        <input type='text'></input>
+        <label>Last Name</label>
+        <input type='text'></input>
+        <label>Email</label>
+        <input type='text'></input>
+        <label>Password</label>
+        <input type='password'></input>
+        </form>
+        <button type='submit'>Go</button>
+        <p>forgot password?<a href="">click on here!!!</a></p>
+    </div>
+    </>)
+
+}
